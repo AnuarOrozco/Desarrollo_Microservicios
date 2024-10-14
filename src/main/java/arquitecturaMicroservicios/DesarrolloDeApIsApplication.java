0260ca4141adef05acad297a5378750f2c97450d
Anuar <anuarorozzco@gmail.com>
2024-10-14 11:21:43 -0600

    init commit

---
+++ src/main/java/arquitecturaMicroservicios/DesarrolloDeApIsApplication.java
@@ -0,0 +1,13 @@
+package arquitecturaMicroservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloDeApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloDeApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arquitecturaMicroservicios/DesarrolloDeApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitecturaMicroservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloDeApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
