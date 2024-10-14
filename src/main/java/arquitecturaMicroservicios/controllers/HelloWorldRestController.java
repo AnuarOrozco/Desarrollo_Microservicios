d64938cdd00f8fcced9304e77c4f2533f00f8e5c
Anuar <anuarorozzco@gmail.com>
2024-10-14 13:33:08 -0600

    added a few implementations to the HelloWorldRestController class and implemented a new class usign PathVariable to make dinamic URLs

--- src/main/java/arquitecturaMicroservicios/controllers/HelloWorldRestController.java
+++ src/main/java/arquitecturaMicroservicios/controllers/HelloWorldRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldRestController {
 
-    @GetMapping("/hello")
+    @GetMapping({"/hello", "/hw", "/hola"})
     public String helloWorld() {
         return "Hello, World!";
     }
