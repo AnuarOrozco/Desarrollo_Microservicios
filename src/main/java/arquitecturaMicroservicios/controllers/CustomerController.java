11398e561df8ded571f6c0618d56c282f5f5c775
Anuar <anuarorozzco@gmail.com>
2024-10-14 17:03:52 -0600

    Created domain/model package, added Customer class to Domain and implemented the Controller. This was made to prove the Layered Architecture

---
+++ src/main/java/arquitecturaMicroservicios/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package arquitecturaMicroservicios.controllers;
+
+import arquitecturaMicroservicios.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(1, "John Doe", "johndoe", "password123"),
+            new Customer(2, "Mr. Sigma", "janesmith", "Whatasigma123"),
+            new Customer(3, "Gonzalo Herrera", "gonzoohh", "xDDDppcdsalv"),
+            new Customer(4, "David Medina", "Eldeivid", "password456")
+    ));
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+}
