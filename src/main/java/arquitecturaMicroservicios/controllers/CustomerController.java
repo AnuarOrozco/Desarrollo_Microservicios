2b7885686ca43ec851a89e625fcd94c00b80cdbc
Anuar <anuarorozzco@gmail.com>
2024-10-15 11:44:24 -0600

    Created and implemented POST Mapping method and RequestBody to allow us to create new customers using PostMan

--- src/main/java/arquitecturaMicroservicios/controllers/CustomerController.java
+++ src/main/java/arquitecturaMicroservicios/controllers/CustomerController.java
@@ -1,9 +1,7 @@
 package arquitecturaMicroservicios.controllers;
 
 import arquitecturaMicroservicios.domain.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,7 +35,11 @@ public class CustomerController {
         return null;
     }
 
-
+    @PostMapping("/customers")
+    public Customer postCustomer(@RequestBody Customer customer) {
+        customers.add(customer);
+        return customer;
+    }
 
 
 
