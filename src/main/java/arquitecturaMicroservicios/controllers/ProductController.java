cd2aea22fac1a36939c075675ea8db6fd1b604a3
Anuar <anuarorozzco@gmail.com>
2024-10-22 10:27:00 -0600

    Implemented dependency injection and created a service bean

--- src/main/java/arquitecturaMicroservicios/controllers/ProductController.java
+++ src/main/java/arquitecturaMicroservicios/controllers/ProductController.java
@@ -3,6 +3,7 @@ package arquitecturaMicroservicios.controllers;
 import arquitecturaMicroservicios.domain.Product;
 import arquitecturaMicroservicios.service.ProductService;
 import arquitecturaMicroservicios.service.ProductsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,10 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    ProductService productsService = new ProductsServiceImpl();
+    // Class instance
+    //ProductService productsService = new ProductsServiceImpl();
+    @Autowired
+    private ProductService productsService;
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
